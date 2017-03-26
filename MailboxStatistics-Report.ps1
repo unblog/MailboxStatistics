e84e3e859afbc7da395bb4b9ea183ccef4a0deda
Don Matteo <donkey.shark@gmail.com>
2017-03-26 19:17:38 +0200

    Update MailboxStatistics-Report.ps1

--- MailboxStatistics-Report.ps1
+++ MailboxStatistics-Report.ps1
@@ -10,21 +10,22 @@ $exch = [system.environment]::MachineName
 $build = (Get-ExchangeServer -Identity $env:exch | ft name,AdminDisplayVersion -HideTableHeaders -AutoSize)
 $path = [Environment]::GetFolderPath('ApplicationData')
 $file = "$path\report.txt"
-$mailboxdata = (Get-MailboxStatistics -Server $exch | Sort-Object TotalItemSize -Descending | ft DisplayName,@{label=�TotalItemSize(MB)�;expression={$_.TotalItemSize.Value.ToMB()}},ItemCount,LastLogonTime -auto)
+$mailboxdata = (Get-MailboxStatistics -Server $exch | Sort-Object TotalItemSize -Descending | ft DisplayName,@{label="TotalItemSize(MB)";expression={$_.TotalItemSize.Value.ToMB()}},ItemCount,LastLogonTime -auto)
 $HealthReport = (Get-HealthReport -Server $exch | Where-Object { $_.alertvalue -ne "Healthy" })
+$disk = (Get-WMIObject Win32_Logicaldisk -ComputerName $exch | Select PSComputername,DeviceID,@{Name="SizeGB";Expression={$_.Size/1GB -as [int]}},@{Name="FreeGB";Expression={[math]::Round($_.Freespace/1GB,2)}})
 $date | Out-File $file
 $build | Out-File $file -append
 $mailboxdata | Out-File $file -append
 $HealthReport | Out-File $file -append
+$disk | Out-File $file -append
 $smtpServer = "127.0.0.1"
 $att = new-object System.Net.Mail.Attachment($file)
 $msg = new-object System.Net.Mail.MailMessage
 $smtp = new-object System.Net.Mail.SmtpClient($smtpServer)
-$msg.From = "postmaster@email.com"
+$msg.From = "no_reply@$exch"
 $msg.To.Add("your@email.com")
-$msg.To.Add("cc.recipient@email.com")
 $msg.Subject = "Notification from $exch"
-$msg.Body = "$exch MailboxStatistics generated $date attachment mailbox statistics reporting to $file"
+$msg.Body = "$exch MailboxStatistics generated $date reported to attachment $file"
 $msg.Attachments.Add($att)
 $smtp.Send($msg)
 $att.Disposen
